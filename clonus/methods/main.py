87cd085ab08ce875effdde17cbbc5f38389a073e
Andrey <andrewnureev142@gmail.com>
2023-05-22 16:43:32 +0300

    added ~clowns~ clonus

--- clonus/methods/main.py
+++ clonus/methods/main.py
@@ -3,7 +3,7 @@ from method_configurator import MethodConfigurator
 
 
 def main():
-    filenames = ['./Tests/Python/test6.py', './Tests/Python/test7.py']
+    filenames = ['./clonus/Tests/Python/test6.py', './clonus/Tests/Python/test7.py']
     fp_builder = FingerprintMethodBuilder(filenames)
     config = MethodConfigurator(fp_builder)
     res = config.make_method()
--- clonus/views.py
+++ clonus/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
 # Create your views here.
+
+def index(request: HttpRequest):
+    return render(request, 'index.html')
+
+def f2f(request: HttpRequest):
+    pass
+
+def f2db(request: HttpRequest):
+    pass
