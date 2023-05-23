7293869548c50225dc910b5604c0a8386f2cd8fe
Andrey <andrewnureev142@gmail.com>
2023-05-23 22:21:26 +0300

    added plagirism highlighting

--- clonus/views.py
+++ clonus/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from pathlib import Path
+from collections import deque
 
 from clonus.forms import FileToFileForm
 from clonus.models import Package
@@ -46,37 +47,65 @@ def summary(request: HttpRequest, h: str):
         p = Package.objects.get(hash=h)
     except Package.DoesNotExist:
         return redirect("index")
-    if not p.processed:
-        filenames = [p.file1, p.file2]
-        fp_builder = FingerprintMethodBuilder(filenames, p.gram_size, p.window_size)
-        config = MethodConfigurator(fp_builder)
-        method_res = config.make_method()
-        # method_res.print()
-        # p.coeff = round(method_res.clone_pct * 100,2)
-        p.coeff = method_res.clone_pct
-        p.processed = True
-        p.save()
-    
-    with open(p.file1, 'r', encoding='utf-8') as f:
-        file1 = f.read()
-    
-    with open(p.file2, 'r', encoding='utf-8') as f:
-        file2 = f.read()
+
+    filenames = [p.file1, p.file2]
+    fp_builder = FingerprintMethodBuilder(filenames, p.gram_size, p.window_size)
+    config = MethodConfigurator(fp_builder)
+    method_res = config.make_method()
+    p.coeff = method_res.clone_pct
+    p.processed = True
+    p.save()
+
+    def process_file(fil: Path, l: "list[list[int]]"):
+        res = []
+        pos = 0
+        contents = ""
+        line_count = 0
+        offsets = deque(l)
+        cur = offsets[0][0]
+        end_of_q = False
+        with open(fil, "r", encoding="utf-8") as f:
+            for line in f:
+                contents += line
+                line_count += 1
+                pos += len(line)
+                if pos >= cur and not end_of_q:
+                    if cur == offsets[0][0]:
+                        cur = offsets[0][1]
+                        res.append([line_count, line_count])
+                    else:
+                        offsets.popleft()
+                        try:
+                            cur = offsets[0][0]
+                        except IndexError:
+                            end_of_q = True
+                        res[-1][-1] = line_count
+            # print(res)
+        return contents, res
+
+    file1, q1 = process_file(p.file1, method_res.clone_parts_1)
+    file2, q2 = process_file(p.file2, method_res.clone_parts_2)
 
     context = {
         "hash": p.hash,
-        "coeff": p.coeff*100,
+        "coeff": p.coeff * 100,
         "file1": file1,
         "file2": file2,
         "f1": Path(p.file1).name,
-        "f2": Path(p.file2).name
+        "f2": Path(p.file2).name,
+        "q1": ", ".join(
+            "{start: " + str(i) + ", end: " + str(j) + ", color: 'yellow'}"
+            for i, j in q1
+        ),
+        "q2": ", ".join(
+            "{start: " + str(i) + ", end: " + str(j) + ", color: 'yellow'}"
+            for i, j in q2
+        ),
     }
     return render(request, "summary.html", context)
 
 
 def list(request: HttpRequest):
     q = Package.objects.all().order_by("-date")
-    context = {
-        "packages": ((i, Path(i.file1).name, Path(i.file2).name) for i in q)
-    }
+    context = {"packages": ((i, Path(i.file1).name, Path(i.file2).name) for i in q)}
     return render(request, "list.html", context)
